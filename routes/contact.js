9c7d65b97be3cda2aae5fd64ece316ecc140e6bb
0script <59306978+0script@users.noreply.github.com>
2022-03-08 15:41:17 +0000

    Update contact.js
    Accidentally publish gmail credential

--- routes/contact.js
+++ routes/contact.js
@@ -15,14 +15,14 @@ router.post('/send',function(req,res,next){
     port: 465,
     secure: true,
     auth: {
-      user: 'zokme00@gmail.com',
-      pass: 'z0kme 00 g00gle.com',
+      user: 'xxxxxxx@gmail.com',
+      pass: 'xxxxxxxx',
     }
   });
 
   const mailOptions={
     from: 'John Doe <johndoe@outlook.com>',
-    to: 'zokme00@gmail.com',
+    to: 'xxxxxxx@gmail.com',
     subject: 'Website Submition',
     text: 'You have a new submtion with the followind details... Name: '+req.body.name+' Email : '+req.body.email+'Message :'+req.body.message,
     html:'<h2>New Submission</h2><ul><li> Name: '+req.body.name+'</li><li> Email : '+req.body.email+'</li><li> Message : '+req.body.message+'</li></ul>'
